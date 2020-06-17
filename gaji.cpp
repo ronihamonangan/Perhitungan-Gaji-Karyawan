db2953ceccca9436b53589812221c8fce0dd097c
ronihamonangan <ronihamonangan93@students.unnes.ac.id>
2020-06-17 19:09:09 +0700

    Semoga bisa selesai

--- gaji.cpp
+++ gaji.cpp
@@ -1,5 +1,6 @@
 #include "gaji.h"
 #include "ui_gaji.h"
+#include <QString>
 
 gaji::gaji(QWidget *parent) :
     QDialog(parent),
@@ -15,5 +16,16 @@ gaji::~gaji()
 
 void gaji::on_pushButton_clicked()
 {
+    QString gajipokok = ui->lineEdit_5->text();
+    QString tunjangan = ui->lineEdit_6->text();
+  //QString jabatan = ui->lineEdit_3->text();
+    QString jabatan = ui->comboBox->currentText();
+  //QString status = ui->lineEdit_4->text();
+    QString status = ui->comboBox_2->currentText();
+    QString gajikotor = ui->lineEdit_7->text();
+
+    ui->lineEdit_2->setText(ui->lineEdit->text());
+    ui->lineEdit_3->setText(ui->comboBox->itemText(ui->comboBox->currentIndex()));
+    ui->lineEdit_4->setText(ui->comboBox_2->itemText(ui->comboBox_2->currentIndex()));
 
 }
